cf0c6bb393b3c75816d81764ae846a1d2c43a4b9
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-06-01 23:22:48 +0900

    プロジェクト追加

---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public List<GameObject> enemy = new List<GameObject>();
+    public GameObject prefabEnemy;
+    public void Add()
+    {
+        GameObject goEnemy = Instantiate(prefabEnemy) as GameObject;
+        this.enemy.Add(goEnemy);
+        goEnemy.name = $"enemy_{enemy.Count}";
+    }
+    private void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Add();
+        }
+    }
+    private void Update()
+    {
+        if( this.gameObject == null)
+        {
+
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(enemy[1]);
+            enemy.Remove(enemy[1]);
+        }
+    }
+
+}
---
+++ Assets/ListTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void aStart()
+    {
+        //EnemyGenerator.enemy.Add(this.gameObject);
+        Destroy(this.gameObject);
+        //EnemyGenerator.enemy.Remove(this.gameObject);
+    }
+
+
+}
